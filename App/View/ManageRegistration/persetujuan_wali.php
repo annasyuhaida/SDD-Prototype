b6053cdb5b229feb3853e4b365c1ba4eea8d3fe5
blqsaqla02 <133661551+blqsaqla02@users.noreply.github.com>
2023-06-15 01:44:30 +0800

    update

--- App/View/ManageRegistration/persetujuan_wali.php
+++ App/View/ManageRegistration/persetujuan_wali.php
@@ -1,4 +1,4 @@
-<?php
+<!-- php
 
 class persetujuan_wali {
     public function render() {
@@ -115,7 +115,114 @@ class persetujuan_wali {
   </script>
 </body>
 </html>
+
+-->
+
 <?php
+
+class persetujuan_wali {
+    public function render() {
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+</html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+  <style>
+    body {
+      padding: 20px;
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+    }
+
+    .heading {
+      background-color: #004080;
+      color: #fff;
+      padding: 10px;
+      text-align: center;
+      margin-bottom: 20px;
+    }
+
+    .dark-blue-box {
+      border: 2px solid #004080;
+      background-color: #004080;
+      color: #fff;
+      padding: 20px;
+      width: 400px;
+      margin: 0 auto;
+      box-sizing: border-box;
+      margin-bottom: 20px;
+    }
+
+    .form-label {
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    .button {
+      background-color: #ffd700;
+      color: #000;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      margin-right: 10px;
+    }
+  </style>
+</head>
+<body>
+  <div class="heading">
+    <h2>Persetujuan Wali</h2>
+  </div>
+
+  <div class="dark-blue-box">
+  <form action="index.php?action=createDoc" method="POST">
+  <label class="form-label">
+        Saya
+        <input type="text" name="waliname" required>
+      </label>
+      <label class="form-label">
+        No. Kad Pengenalan/Pasport:
+        <input type="text" name="waliIc" required>
+      </label>
+      <label class="form-label">
+        adalah
+        <input type="text" name="relationship" required>
+      </label>
+      <label class="form-label">
+        kepada
+        <input type="text" name="bridename" required>
+      </label>
+      <label class="form-label">
+        dengan ini membuat pengakuan bahawa saya bersetuju dengan perkahwinan di antara
+        <input type="text" name="bridename" required>
+        dengan
+        <input type="text" name="groomname" required>
+        dengan mas kahwinnya RM
+        <input type="number" name="dowry" required>
+        *tunai/hutang. Saya akan mengakad nikahkan sendiri perkahwinan ini. Tarikh
+        <input type="text" name="date" required>
+        .
+      </label>
+
+                            
+                            <input type="save" class="button" value="save">
+                        </form>
+                    </div>
+                </div>
+
+
+
+            </body>
+
+            </html>
+
+    <?php
     }
 }
-        ?>
+    ?>
